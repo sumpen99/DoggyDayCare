9f5f4affd05373b603a5fca679a6adce40d17a90
sumpen99 <sumpen841001@hotmail.com>
2023-10-02 17:54:45 +0200

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function RoutesWithAnimationAndRoutClients({setHiddenMenu}) {
 
 function RoutesWithAnimationAndSheetClients() {
   const location = useLocation();
-  
+  console.log(location);
   return (
     <Routes location={location} key="default">
       <Route path="/" element={<Home />} />
@@ -59,7 +59,7 @@ function App() {
     return (
       <div className="App">
       <LoaderProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="">
         <NavBar/>
           <LocationProvider>
             <RoutesWithAnimationAndSheetClients/>
