db1e9cbb6db6c8c7247fce258be295c918782071
sumpen99 <sumpen841001@hotmail.com>
2023-10-02 06:59:45 +0200

    error and contact page setup

--- src/helper/clientDataHandler.jsx
+++ src/helper/clientDataHandler.jsx
@@ -2,7 +2,7 @@ import { FILTER_OPTION,stringInterPolation } from "./core";
 let cache = null;
 const baseUrl = 'https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683';
 
-export let lastRequest = { 
+let lastRequest = { 
   filterOption: null,
   perPageOption:null,
   valueToMatch: null,
